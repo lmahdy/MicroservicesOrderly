4dedd16147cd5db1c85ce81cc3bb79ada2ee79cd
nour233 <mohamednour.slimi@esprit.tn>
2026-02-22 13:35:54 +0000

    Revert "Merge branch 'product-service' into main"
    This reverts commit 29c7405345bbc4ee65e7f9ce6c88e2ad22e6c352, reversing
    changes made to a1f4f2b3013243ce5f3500bccaf861ac609c2142.

--- src/legacy/jobms/JobRepository.java
+++
@@ -1,8 +0,0 @@
-package esprit.jobms;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JobRepository extends  JpaRepository<Job , Integer> {
-}
--- src/main/java/com/orderly/product/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.orderly.product.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
